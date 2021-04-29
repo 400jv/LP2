da912744546b616cbec48ec31f0d4e9b08c00f54
João Victor <joaovictorramalho7@gmail.com>
2021-04-29 16:16:56 -0300

    Build a class in Java that asks the user to type text in capital letters and show lowercase text, then request lowercase text and show it in capital letters. (Use the toLowerCase () and toUpperCase () methods).

---
+++ string/Ex3.java
@@ -0,0 +1,10 @@
+public class Ex3 {
+  
+  public String paraMaiusculo(String textoMinusculo) {
+    return textoMinusculo.toUpperCase();
+  }
+
+  public String paraMinusculo(String textoMaiusculo) {
+    return textoMaiusculo.toLowerCase();
+  }
+}
