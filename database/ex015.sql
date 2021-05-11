5795621a68c62818999b43a403c2c720e57ea24e
João Victor <joaovictorramalho7@gmail.com>
2021-05-11 16:16:14 -0300

    Ex015

---
+++ database/ex015.sql
@@ -0,0 +1,13 @@
+/* Ex 6 */
+
+/*
+  Selecione o nome dos atores que trabalharam nas mesmas novelas que a
+  atriz “Joaquina Penteado”.
+*/
+
+SELECT nome
+FROM Ator
+INNER JOIN Novela
+WHERE (
+
+)  
