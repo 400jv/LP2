af1a6c3e899ebd19b3b99ca4d0a0aa479dd30b5c
João Victor <joaovictorramalho7@gmail.com>
2021-05-13 16:24:33 -0300

    Ex010

--- database/ex010.sql
+++ database/ex010.sql
@@ -1,8 +1,10 @@
 /* Ex 1 */
 
-SELECT * 
+SELECT a.nome
 FROM 
   Ator AS a
-  INNER JOIN Novela AS n ON a.id=  n.Personagem_id
+  INNER JOIN Personagem AS p ON p.ator_id = a.id
+  INNER JOIN Novela_personagem AS np ON np.Personagem_id = p.id
+  INNER JOIN Novela AS n ON n.id = np.novela_id
 WHERE 
-  n.horario = 18;
+  n.horario = '18:00:00';
