6a5a7f7dce6c68dc615dadc5b50844c8518abe53
João Victor <joaovictorramalho7@gmail.com>
2021-05-14 16:53:36 -0300

    Write a Java code that displays the values ​​of an array to double in the same line

---
+++ array/src/Ex2.java
@@ -0,0 +1,11 @@
+public class Ex2 {
+  public static void main(String[] args) {
+    Double[] array = { 1.2, 0.3222222, 5498.40 };
+
+    System.out.println("Exercício 2: ");
+
+    for (Double value: array) {
+      System.out.print(value + " ");
+    }
+  }
+}
