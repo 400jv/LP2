49b62d9808800e39be3a23f3b38b764a8c35fc66
João Victor <joaovictorramalho7@gmail.com>
2021-06-04 18:18:04 -0300

    Ex5

--- matriz/src/Ex5.java
+++ matriz/src/Ex5.java
@@ -2,7 +2,7 @@ public class Ex5 {
   public static void main(String[] args) {
     double[][] matriz = new double[4][5];
 
-    System.out.println("Ex 4) Quantidades de colunas: " + matrizDoubleQuantidadeColunas(matriz));
+    System.out.println("Ex 5) Quantidade de colunas: " + matrizDoubleQuantidadeColunas(matriz));
   }
 
   public static int matrizDoubleQuantidadeColunas(double[][] matriz) {
