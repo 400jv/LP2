eeda477eff1d16e379f670be02170284cb236776
João Victor <joaovictorramalho7@gmail.com>
2021-06-04 00:14:23 -0300

    Ex2

---
+++ matriz/src/Ex2.java
@@ -0,0 +1,17 @@
+import java.lang.Math;
+
+public class Ex2 {
+  public static void main(String[] args) {
+    int[][] matriz = new int[4][4];
+
+    System.out.println("2)");
+
+    for (int i = 0; i < matriz.length; i++) {
+      for (int j = 0; j < 4; j++) {
+        matriz[i][j] = (int) (Math.random() * 100);
+      }
+    }
+
+    Util.printIntMatriz(matriz);
+  }
+}
